3759923bcd6f45afd7bc4e7825c6e51cd99a0dba
Richard Pereira do Nascimento <richard.nascimento@ilia.digital>
2022-01-20 20:43:19 -0300

    :tada: Init project

---
+++ src/main/java/com/scrum/planningandretro/PlanningAndRetroApplication.java
@@ -0,0 +1,13 @@
+package com.scrum.planningandretro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningAndRetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningAndRetroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrum/planningandretro/PlanningAndRetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrum.planningandretro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningAndRetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
