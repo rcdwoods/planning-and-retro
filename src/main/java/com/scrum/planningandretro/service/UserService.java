937deca2ffd34cc5af022ae4477cbe313ba26744
Richard Pereira do Nascimento <richard.nascimento@ilia.digital>
2022-01-25 15:26:00 -0300

    :rotating_light: Remove unused import

--- src/main/java/com/scrum/planningandretro/service/UserService.java
+++ src/main/java/com/scrum/planningandretro/service/UserService.java
@@ -4,7 +4,6 @@ import com.scrum.planningandretro.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
